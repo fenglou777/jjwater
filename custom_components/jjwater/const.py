9a9046865fb1ea3bc6179a54619070c777ddf945
fenglou777 <85816450+fenglou777@users.noreply.github.com>
2026-08-04 11:46:59 +0800

    Add constant definitions for jjwater component

---
+++ custom_components/jjwater/const.py
@@ -0,0 +1,9 @@
+"""晋江水务 常量定义"""
+DOMAIN = "jjwater"
+CONF_TOKEN = "token"
+CONF_ACCOUNTS = "accounts"
+
+API_URL_DAILY = "https://wwt.jinjiangwater.com/jjapis/ysBase/findEveryDayYsl"
+API_URL_YEAR = "https://wwt.jinjiangwater.com/jjapis/ysBase/findYearDz"
+
+DEFAULT_UPDATE_INTERVAL = 18000  # 默认刷新频率：5小时
